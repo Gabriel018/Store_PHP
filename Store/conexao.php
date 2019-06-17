0cad266654c7fc73b8d3ed5603ddab08da4bd0cc
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2019-06-17 20:43:41 -0300

    Add files via upload

---
+++ Store/conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha 	  = '';
+$banco 	  = 'storephp';
+//mysqli_connect serve para conectar os arquivos php bd
+$conexao  = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+?>
